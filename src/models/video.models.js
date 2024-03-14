58c1439de213387d10021d23b5df5c8b00b29c92
gulamyazdani <gulamyazdani03@gmail.com>
2024-03-14 15:15:25 +0530

    Created Video Model

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -2,7 +2,38 @@ import mongoose, { Schema } from "mongoose";
 
 const videoSchema = new Schema(
     {
-        
+       videoFile: {
+        type: String, // Cloudinary url
+        required: true
+       },
+       thumbnail: {
+        type: String, // Cloudinary url
+        required: true
+       },
+       title: {
+        type: String,
+        required: true
+       },
+       description: {
+        type: String,
+        required: true
+       },
+       duration: {
+        type: Number, // Cloudinary url
+        required: url
+       },
+       views: {
+        type: Number,
+        default: 0
+       },
+       isPublished: {
+        type: boolean,
+        default: true
+       },
+       owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+       }
     },
     {
         timestamps: true
